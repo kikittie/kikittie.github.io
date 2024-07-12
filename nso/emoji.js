a1a054c7bfc20c966fd2827821618d4dad02899b
𝑘𝑖𝑡𝑡𝑦 <catherinepewpew@gmail.com>
2024-07-12 12:18:10 +0100

    Update emoji.js

--- nso/emoji.js
+++ nso/emoji.js
@@ -29,7 +29,7 @@ function loadEmojis(){
 		var inner = this.nodeValue;
 		for(var i = 0; i < emojis.length; ++i){
 			var regex = new RegExp((emojis[i]+'((?!\.gif))'), "g");
-			var img = '<img src="https://raw.githubusercontent.com/kikittie/nso-emojis/main/'+emojis[i]+'.png" style="user-drag: none; -webkit-user-drag: none; user-select: none; -moz-user-select: none; -webkit-user-select: none; -ms-user-select: none;"></img>';
+			var img = '<img src="https://kikittie.github.io/nso/'+emojis[i]+'.png" style="user-drag: none; -webkit-user-drag: none; user-select: none; -moz-user-select: none; -webkit-user-select: none; -ms-user-select: none;"></img>';
 			inner = inner.replace(regex, img);
 		}
 		return inner;
