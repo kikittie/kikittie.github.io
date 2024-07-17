ae88a7702c312c59f74daa306768ea26d370e399
𝑘𝑖𝑡𝑡𝑦 <catherinepewpew@gmail.com>
2024-07-17 13:12:59 +0100

    Update emoji.js

--- nso/emoji.js
+++ nso/emoji.js
@@ -4,11 +4,11 @@ whiteoverlay.style.position = 'fixed';
 whiteoverlay.style.display = 'flex';
 whiteoverlay.style.justifyContent = 'center';
 whiteoverlay.style.alignItems = 'center';
-whiteoverlay.style.width = '20px';
-whiteoverlay.style.height = '20px';
+whiteoverlay.style.width = '100%';
+whiteoverlay.style.height = '100%';
 whiteoverlay.style.top = '0';
 whiteoverlay.style.left = '0';
-whiteoverlay.style.background = '#fff';
+whiteoverlay.style.background = '#fff0';
 whiteoverlay.style.zIndex = '999999';
 if(!$('#emojis_whiteoverlay').length){
 	$(whiteoverlay).insertAfter('head');
@@ -29,7 +29,7 @@ function loadEmojis(){
 		var inner = this.nodeValue;
 		for(var i = 0; i < emojis.length; ++i){
 			var regex = new RegExp((emojis[i]+'((?!\.gif))'), "g");
-			var img = '<img src="https://kikittie.github.io/nso/'+emojis[i]+'.png" style="user-drag: none; -webkit-user-drag: none; user-select: none; -moz-user-select: none; -webkit-user-select: none; -ms-user-select: none;"></img>';
+			var img = '<img src="https://kikittie.github.io/nso/'+emojis[i]+'.png" style="user-drag: none; -webkit-user-drag: none; user-select: none; -moz-user-select: none; -webkit-user-select: none; -ms-user-select: none;" height="20px" width="20px"></img>';
 			inner = inner.replace(regex, img);
 		}
 		return inner;
