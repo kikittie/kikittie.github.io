0ab8504aafa2a388c18d28cd6e08dadc3189f90a
𝑘𝑖𝑡𝑡𝑦 <catherinepewpew@gmail.com>
2025-07-19 12:34:08 +0100

    Update emojis.js

--- emojis.js
+++ emojis.js
@@ -18,7 +18,7 @@ if(!$('#emojis_whiteoverlay').length){
 }
 
 function loadEmojis(){
-	var emojis = ['🍮','👀','✝️','🧷','🕷️','🗝️'];
+	var emojis = ['🍮','👀','✝️','🧷','🕷️','🗝️','🤎','🚘'];
 	$('script').each(function(){
 		if(!$(this).parent('textarea').length){
 			var inner = $(this).html();
